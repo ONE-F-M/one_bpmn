d0df9186bfe30347b0449fd252b2b2cb5c483981
samdanikouser <samdani.shareef6@gmail.com>
2026-04-02 00:50:02 +0300

    hooks and process json file update

--- one_bpmn/public/processa/assets/InsertLink-5b2f08bd.js
+++
@@ -1,2 +0,0 @@
-import{c5 as t,bZ as s,b_ as a,b$ as i}from"./index-d9697920.js";const d=t({__name:"InsertLink",props:{editor:{}},setup(o){const e=o;function n(){e.editor.commands.openLinkEditor()}return(r,p)=>s(r.$slots,"default",a(i({onClick:n})))}});export{d as default};
-//# sourceMappingURL=InsertLink-5b2f08bd.js.map
