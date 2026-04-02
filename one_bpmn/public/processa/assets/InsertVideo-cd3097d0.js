6b3a898171699ceae0a9d107f8e320dd6f76901f
samdanikouser <samdani.shareef6@gmail.com>
2026-04-02 17:09:14 +0300

    Remove processa assets changes from PR

--- one_bpmn/public/processa/assets/InsertVideo-cd3097d0.js
+++
@@ -1,2 +0,0 @@
-import{c5 as s,bZ as t,b_ as a,b$ as d}from"./index-ed28c02a.js";const p=s({__name:"InsertVideo",props:{editor:{}},setup(e){const o=e;function n(){o.editor.chain().focus().selectAndUploadVideo().run()}return(r,c)=>t(r.$slots,"default",a(d({onClick:n})))}});export{p as default};
-//# sourceMappingURL=InsertVideo-cd3097d0.js.map
