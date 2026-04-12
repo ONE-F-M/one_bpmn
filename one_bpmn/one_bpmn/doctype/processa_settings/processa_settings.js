c3159b2c2f1119d0bf74eef393c77213ad1e46b5
abraham_adekunle <gadekunle@afexnigeria.com>
2026-04-12 09:01:50 +0100

    added processa settings

---
+++ one_bpmn/one_bpmn/doctype/processa_settings/processa_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, One BPMN and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Processa Settings", {
+	refresh(frm) {
+		// Initialization code if needed
+	},
+});
